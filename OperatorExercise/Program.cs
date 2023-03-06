f9a6dde909736cc637e5c91b9dda2b90128ae915
Alejandro Alatorre <alejandro.c.alatorre@gmail.com>
2023-03-06 11:30:41 -0800

    Operator ex

---
+++ OperatorExercise/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace OperatorExercise
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 17;
+            int b = 4;
+
+
+            Console.WriteLine($"A: 17");
+            Console.WriteLine($"B: 4");
+
+            Console.WriteLine($"A plus B = {a + b}.");
+
+            Console.WriteLine($"A minus B = {a - b}.");
+
+            Console.WriteLine($"A times B = {a * b}.");
+
+            Console.WriteLine($"A ({a}) divided by B ({4}) = {a / b} with remainder of {a % b}.");
+            Console.WriteLine();
+           
+            Console.WriteLine("What is the radius of your circle?");
+            var radius = double.Parse(Console.ReadLine());
+
+            double pi = 3.14;
+            double areaOfCircle = pi * (radius * radius);
+            Console.WriteLine($"The area of your circle with a radius of {radius} is {areaOfCircle}");
+
+        }
+    }
+}
